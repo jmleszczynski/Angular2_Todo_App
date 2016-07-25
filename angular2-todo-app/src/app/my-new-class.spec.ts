0310e0894d1b06e6599f5b3e44905db12f5b4755
jmleszczynski <jmleszczynski@gmail.com>
2016-07-24 21:12:19 -0400

    Added Pipe, Class, Service, Interface, &  Enum
    Added basic building blocks for app

---
+++ angular2-todo-app/src/app/my-new-class.spec.ts
@@ -0,0 +1,10 @@
+/* tslint:disable:no-unused-variable */
+
+import { addProviders, async, inject } from '@angular/core/testing';
+import {MyNewClass} from './my-new-class';
+
+describe('MyNewClass', () => {
+  it('should create an instance', () => {
+    expect(new MyNewClass()).toBeTruthy();
+  });
+});
